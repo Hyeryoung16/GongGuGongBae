ba394add59495041f0cb56fea3391dbfbf2179c5
Hyeryoung16 <hyeryoung16@gmail.com>
2022-03-02 20:01:30 +0900

    first commit

---
+++ src/main/java/hello/gonggugongbae/GongguGongbaeApplication.java
@@ -0,0 +1,13 @@
+package hello.gonggugongbae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongguGongbaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongguGongbaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/gonggugongbae/GongguGongbaeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.gonggugongbae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongguGongbaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

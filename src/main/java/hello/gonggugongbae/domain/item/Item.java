4cf7ee9107d4b625c6f37561cb132c308ea74c6f
Hyeryoung16 <hyeryoung16@gmail.com>
2022-03-28 19:27:33 +0900

    [ADD CODE] Item Domain - Item class

--- src/main/java/hello/gonggugongbae/domain/item/Item.java
+++ src/main/java/hello/gonggugongbae/domain/item/Item.java
@@ -1,2 +1,8 @@
-package hello.gonggugongbae.domain.item;public class Item {
+package hello.gonggugongbae.domain.item;
+
+public class Item {
+    private String itemName; // 구매처
+    private String refLink; // 참고링크
+    private int minOrderPrice; // 최소주문금액
+    private int deliveryPrice; // 배송(배달)비
 }

0ed8c7a4edcc33d75803da44cd745836890dea83
Hyeryoung16 <hyeryoung16@gmail.com>
2022-03-29 00:11:16 +0900

    [MODIFY CODE] Location Domain - LocationPolicy Impl class

--- src/main/java/hello/gonggugongbae/domain/location/LocationPolicyImpl.java
+++ src/main/java/hello/gonggugongbae/domain/location/LocationPolicyImpl.java
@@ -1,8 +1,44 @@
 package hello.gonggugongbae.domain.location;
 
 public class LocationPolicyImpl implements LocationPolicy{
+
+    private double maxDistance = 1000;
     @Override
     public boolean isNearLocation(Location loc1, Location loc2) {
-        return true; // TODO : 일단 어떤 위치든 상관없이 허용
+        double dist = distance(loc1, loc2);
+        if (dist <= maxDistance) {
+            return true; // loc2가 loc1의 반경 1000m 이내일 경우
+        }
+        else {
+            return false;
+        }
+    }
+
+    private static double distance(Location loc1, Location loc2) {
+        double lon1 = loc1.getLongitude();
+        double lon2 = loc2.getLongitude();
+        double lat1 = loc1.getLatitude();
+        double lat2 = loc2.getLatitude();
+
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        // to Meter
+        dist = dist * 1609.344;
+        return dist;
+    }
+
+    // This function converts decimal degrees to radians
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    // This function converts radians to decimal degrees
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 }
--- src/main/java/hello/gonggugongbae/domain/location/MyLocation.java
+++ src/main/java/hello/gonggugongbae/domain/location/MyLocation.java
@@ -1,2 +1,12 @@
-package hello.gonggugongbae.domain.location;public class MyLocation {
+package hello.gonggugongbae.domain.location;
+
+public class MyLocation {
+    public static final double GYM_LAT = 37.513719607210334;
+    public static final double GYM_LON = 126.76319380714517;
+
+    public static final double PARK_LAT = 37.512213851401348;
+    public static final double PARK_LON = 126.75918206537753;
+
+    public static final double STATION_LAT = 37.50659571543858;
+    public static final double STATION_LON = 126.7420362291422;
 }

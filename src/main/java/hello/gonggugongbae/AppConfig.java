ae345fb289ad4f4ab334643ff867a7ca39b8029e
Hyeryoung16 <hyeryoung16@gmail.com>
2022-03-29 17:50:02 +0900

    [MODIFY CODE] AppConfig refactoring

--- src/main/java/hello/gonggugongbae/AppConfig.java
+++ src/main/java/hello/gonggugongbae/AppConfig.java
@@ -1,24 +1,39 @@
 package hello.gonggugongbae;
 
+import hello.gonggugongbae.domain.location.LocationPolicy;
 import hello.gonggugongbae.domain.location.LocationPolicyImpl;
 import hello.gonggugongbae.domain.location.LocationPolicyImplV2;
+import hello.gonggugongbae.domain.member.MemberRepository;
 import hello.gonggugongbae.domain.member.MemberRepositoryMemory;
 import hello.gonggugongbae.domain.member.MemberService;
 import hello.gonggugongbae.domain.member.MemberServiceImpl;
+import hello.gonggugongbae.domain.party.PartyRepository;
 import hello.gonggugongbae.domain.party.PartyRepositoryMemory;
 import hello.gonggugongbae.domain.party.PartyService;
 import hello.gonggugongbae.domain.party.PartyServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemberRepositoryMemory());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public PartyService partyService(){
         return new PartyServiceImpl(
-                new MemberRepositoryMemory(),
-                new PartyRepositoryMemory(),
-                new LocationPolicyImpl()
+                memberRepository(),
+                partyRepository(),
+                locationPolicy()
         );
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemberRepositoryMemory();
+    }
+
+    public PartyRepository partyRepository(){
+        return new PartyRepositoryMemory();
+    }
+
+    public LocationPolicy locationPolicy() {
+        return new LocationPolicyImpl();
     }
 }

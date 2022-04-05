4fe7ac617fb113fc5c388bf455e4f1c1410e74da
Hyeryoung16 <hyeryoung16@gmail.com>
2022-04-06 00:02:41 +0900

    [ADD CODE] PartyApp - Apply Spring Container

--- src/main/java/hello/gonggugongbae/PartyApp.java
+++ src/main/java/hello/gonggugongbae/PartyApp.java
@@ -1,6 +1,11 @@
 package hello.gonggugongbae;
 
+import hello.gonggugongbae.domain.item.Item;
+import hello.gonggugongbae.domain.location.Location;
+import hello.gonggugongbae.domain.location.MyLocation;
+import hello.gonggugongbae.domain.member.Member;
 import hello.gonggugongbae.domain.member.MemberService;
+import hello.gonggugongbae.domain.party.Party;
 import hello.gonggugongbae.domain.party.PartyService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,6 +14,16 @@ public class PartyApp {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         PartyService partyService = applicationContext.getBean("partyService", PartyService.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
+        Member member = new Member("userB", new Location(MyLocation.PARK_LAT, MyLocation.PARK_LON));
+        memberService.join(member);
+
+        Item item = new Item("itemA","www.itemlink.com", 8000, 2000);
+
+        Party party = new Party(member.getId(), item, 3, 2, 3000, new Location(MyLocation.GYM_LAT, MyLocation.GYM_LON));
+        Party createdParty = partyService.createParty(party);
+
+        System.out.println("createdParty = " + createdParty);
     }
 }

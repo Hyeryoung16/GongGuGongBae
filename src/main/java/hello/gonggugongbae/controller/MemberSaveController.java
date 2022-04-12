58d9b0555db4ee972e4f98e4b1869fa6ee7754f0
Hyeryoung16 <hyeryoung16@gmail.com>
2022-04-12 23:53:28 +0900

    [ADD CODE] Member Controller - form, save

---
+++ src/main/java/hello/gonggugongbae/controller/MemberSaveController.java
@@ -0,0 +1,26 @@
+package hello.gonggugongbae.controller;
+
+import hello.gonggugongbae.domain.location.Location;
+import hello.gonggugongbae.domain.member.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class MemberSaveController {
+    @RequestMapping("/gggb/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        double lat = Double.parseDouble(request.getParameter("latitude"));
+        double lon = Double.parseDouble(request.getParameter("longitude"));
+        Location location = new Location(lat, lon);
+
+        Member member = new Member(username, location);
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+}
